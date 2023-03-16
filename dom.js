d4c47fbcbe376bb0b097075f47086df98d10731d
Machiwal2404 <89181590+Machiwal2404@users.noreply.github.com>
2023-03-16 12:38:21 +0530

    new commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+//console.dir(document);
+//document.title= 123;
+//console.log(document.all);
+var headerTitle=document.getElementById('header-title');
+headerTitle.style.borderbottom='solid 3px #000';
+
